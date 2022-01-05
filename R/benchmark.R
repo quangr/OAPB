29102e373eae16a6b252e23e325e4a225f500f7b
quangr <quangr@163.com>
2022-01-05 16:24:20 +0800

    加入自述文件

--- R/benchmark.R
+++ R/benchmark.R
@@ -1,8 +1,19 @@
-library(microbenchmark)
+# library(microbenchmark)
+setup=function(){
+  JuliaCall::julia_setup()
+  funv1<<-JuliaCall::julia_eval("function funv1(X,m::Int)
+         combo=collect(Iterators.flatten(map((x) -> (Iterators.product(x, x+1:m)),1:m-1)))
+         commatrix=zeros(length(combo),m)
+         commatrix.= broadcast((x,y,z)->(y[1]==z)-(y[2]==z),commatrix,combo,(1:m)')
+         reduce(hcat,map(x->reduce(vcat,map(t->commatrix*t,x)), eachrow(X)))
+end")
+  funv2<<-JuliaCall::julia_eval("function funv2(X,m::Int)
+         combo=collect(Iterators.flatten(map((x) -> (Iterators.product(x, x+1:m)),1:m-1)))
+         commatrix=zeros(length(combo),m)
+         commatrix.= broadcast((x,y,z)->(y[1]==z)-(y[2]==z),commatrix,combo,(1:m)')
+         reduce(hcat,map(x->reduce(vcat,map(t->commatrix*t,x)), eachrow(X)))
+end")
 
-# testResult = microbenchmark(PSOD.Finite.qnorm.1.constraint1(matrix(c(1,2,3,4,5,6),nrow=3),3),
-#                             func2(matrix(c(1,2,3,4,5,6),nrow=3),3),julia_call("funv2", matrix(c(1,2,3,4,5,6),nrow=3),3L), times = 10)
-#
-# julia_source("./test.jl")
-# invisible(julia_call("funv2", matrix(c(1,2,3,4,5,6),nrow=3),3L))
-# julia_call("fun1", matrix(c(1,2,3,4,5,6),nrow=3),3L)
+  invisible(JuliaCall::julia_call("funv1", matrix(c(1,2,3,4,5,6),nrow=3),3L))
+  invisible(JuliaCall::julia_call("funv2", matrix(c(1,2,3,4,5,6),nrow=3),3L))
+}
